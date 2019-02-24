50459bbf89f09b8740bd1124186f777faa97a563
shessel <shessel@users.noreply.github.com>
2019-02-24 23:01:38 +0100

    add simple gui for sound generation

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,50 @@
 
 #include "synth.h"
 
+void imgui_sound_desc()
+{
+    static sound_desc desc = {
+        0,              // base_sound_id
+        1.0f,           // amplitude
+        440.0f,         // frequency
+        0,              // frequency_modifier_id
+        0,              // amplitude_modifier_id
+        { 0.0f, 1.0f }, // frequency_modifier_params
+        { 0.0f, 1.0f }, // amplitude_modifier_params
+    };
+
+    bool sound_desc_changed = false;
+    ImGui::Text("Wave Form");   
+    ImGui::SameLine();
+    sound_desc_changed |= ImGui::InputScalar("##base_sound_id", ImGuiDataType_U32, &desc.base_sound_id);
+
+    ImGui::Text("Frequency");
+    ImGui::SameLine();
+    sound_desc_changed |= ImGui::InputFloat("##frequency", &desc.frequency);
+    int frequency_modifier_id = desc.frequency_modifier_id;
+    sound_desc_changed |= ImGui::InputInt("##frequency_modifier_id", &frequency_modifier_id);
+    desc.frequency_modifier_id = static_cast<uint8_t>(frequency_modifier_id);
+    sound_desc_changed |= ImGui::SliderFloat("##frequency_modifier_params.begin", &desc.frequency_modifier_params.begin, 0.0f, 1.0f);
+    sound_desc_changed |= ImGui::SliderFloat("##frequency_modifier_params.end", &desc.frequency_modifier_params.end, 0.0f, 1.0f);
+
+    ImGui::Text("Amplitude");
+    ImGui::SameLine();
+    sound_desc_changed |= ImGui::InputFloat("##amplitude", &desc.amplitude);
+    int amplitude_modifier_id = desc.amplitude_modifier_id;
+    sound_desc_changed |= ImGui::InputInt("##amplitude_modifier_id", &amplitude_modifier_id);
+    desc.amplitude_modifier_id = static_cast<uint8_t>(amplitude_modifier_id);
+    sound_desc_changed |= ImGui::SliderFloat("##amplitude_modifier_params.begin", &desc.amplitude_modifier_params.begin, 0.0f, 1.0f);
+    sound_desc_changed |= ImGui::SliderFloat("##amplitude_modifier_params.end", &desc.amplitude_modifier_params.end, 0.0f, 1.0f);
+    
+    if (ImGui::Button("Play Sound"))
+    {
+        int16_t* buffer = synth_generate_sound(&desc, 1);
+        synth_queue_sound(buffer);
+        Sleep(1000);
+        delete[] buffer;
+    }
+}
+
 int main()
 {
     synth_init();
@@ -82,25 +126,25 @@ int main()
                 synth_play();
             }
             {
-                ImGui::BeginChild("stuff", ImVec2(200.0f, 0.0f), true, ImGuiWindowFlags_NoMove | ImGuiWindowFlags_AlwaysUseWindowPadding);
+                //ImGui::BeginChild("stuff", ImVec2(200.0f, 0.0f), true, ImGuiWindowFlags_NoMove | ImGuiWindowFlags_AlwaysUseWindowPadding);
+                //
+                //ImGui::Text("Envelope");                
+                //
+                //bool adsrChanged = false;
+                //adsrChanged |= ImGui::VSliderFloat("##attack", ImVec2(20.0f, 100.0f), &adsr.a, 0.0f, 1.0f);
+                //ImGui::SameLine();
+                //adsrChanged |= ImGui::VSliderFloat("##decay", ImVec2(20.0f, 100.0f), &adsr.d, 0.0f, 1.0f);
+                //ImGui::SameLine();
+                //adsrChanged |= ImGui::VSliderFloat("##sustain", ImVec2(20.0f, 100.0f), &adsr.s, 0.0f, 1.0f);
+                //ImGui::SameLine();
+                //adsrChanged |= ImGui::VSliderFloat("##release", ImVec2(20.0f, 100.0f), &adsr.r, 0.0f, 1.0f);
+                //
+                //if (adsrChanged)
+                //{
+                //    synth_generate(adsr);
+                //}
                 
-                ImGui::Text("Envelope");                
-
-                bool adsrChanged = false;
-                adsrChanged |= ImGui::VSliderFloat("##attack", ImVec2(20.0f, 100.0f), &adsr.a, 0.0f, 1.0f);
-                ImGui::SameLine();
-                adsrChanged |= ImGui::VSliderFloat("##decay", ImVec2(20.0f, 100.0f), &adsr.d, 0.0f, 1.0f);
-                ImGui::SameLine();
-                adsrChanged |= ImGui::VSliderFloat("##sustain", ImVec2(20.0f, 100.0f), &adsr.s, 0.0f, 1.0f);
-                ImGui::SameLine();
-                adsrChanged |= ImGui::VSliderFloat("##release", ImVec2(20.0f, 100.0f), &adsr.r, 0.0f, 1.0f);
-
-                if (adsrChanged)
-                {
-                    synth_generate(adsr);
-                }
-                
-                ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+                //ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
 
                 //const float values[] = { 0.2f, 0.0f, 0.4f, 0.5f };
                 //ImVec2 mousePosRel;
@@ -113,6 +157,11 @@ int main()
                 //ImGui::PlotHistogram("Hist", values, 4, 0, "yadayada", 0.0f, 1.0f, ImVec2(0.0f, 100.0f));
 
                 //ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+                //ImGui::EndChild();
+            }
+            {
+                ImGui::BeginChild("sound generator", ImVec2(800.0f, 800.0f), true, ImGuiWindowFlags_NoMove | ImGuiWindowFlags_AlwaysUseWindowPadding);
+                imgui_sound_desc();
                 ImGui::EndChild();
             }
 
